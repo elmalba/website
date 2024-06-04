547acaaaa09a7534bfa1f81c527f68b4a74f500b
Manuel Alba <malba@mmae.cl>
2024-06-04 12:18:14 -0400

    fix email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
       <h2>Contacto</h2>
       <p>Si estás interesado en trabajar conmigo o quieres saber más, no dudes en contactarme.</p>
       <div className="contact-info">
-        <p>Email: Malba@malba.cl</p>
+        <p>Email: malba@malba.cl</p>
         <p>Github:<a href='https://www.linkedin.com/in/manuel-alba/'> /manuel-alba/ </a></p>
         <p>Linkedin:<a href='https://github.com/elmalba'> /manuel-alba/ </a></p>
   
